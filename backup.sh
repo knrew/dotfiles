1c1f24e4e976a30b9ac24cd411c4b287df067e4f
Ryunosuke <rew.7cc.12.1f@gmail.com>
2020-04-29 02:58:05 +0900

    update

--- backup.sh
+++ backup.sh
@@ -3,14 +3,15 @@
 DOTFILES_DIR=$(cd $(dirname $0);pwd)
 
 mkdir -p ${DOTFILES_DIR}/.config/i3/
-mkdir -p ${DOTFILES_DIR}/.config/i3status/
+#mkdir -p ${DOTFILES_DIR}/.config/i3status/
 
 cp ${HOME}/.bash_profile ${DOTFILES_DIR}
 cp ${HOME}/.bashrc ${DOTFILES_DIR}
 cp ${HOME}/.config/i3/config ${DOTFILES_DIR}/.config/i3/
-cp ${HOME}/.config/i3status/config ${DOTFILES_DIR}/.config/i3status/ 
+#cp ${HOME}/.config/i3status/config ${DOTFILES_DIR}/.config/i3status/ 
 cp ${HOME}/.gitconfig ${DOTFILES_DIR}
 cp ${HOME}/.latexmkrc ${DOTFILES_DIR}
 cp ${HOME}/.vimrc ${DOTFILES_DIR}
 cp ${HOME}/.Xmodmap ${DOTFILES_DIR}
 cp ${HOME}/.screenrc ${DOTFILES_DIR}
+cp ${HOME}/.xinitrc ${DOTFILES_DIR}
--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 DOTFILES_DIR=$(cd $(dirname $0);pwd)
 
 mkdir -p ${HOME}/.config/i3/
-mkdir -p ${HOME}/.config/i3status
-mkdir -p ${HOME}/Documents/screen_log
+#mkdir -p ${HOME}/.config/i3status
+#mkdir -p ${HOME}/Documents/screen_log
 
 cp ${DOTFILES_DIR}/.bash_profile ${HOME}
 cp ${DOTFILES_DIR}/.bashrc ${HOME}
 cp ${DOTFILES_DIR}/.config/i3/config ${HOME}/.config/i3
-cp ${DOTFILES_DIR}/.config/i3status/config ${HOME}/.config/i3status 
+#cp ${DOTFILES_DIR}/.config/i3status/config ${HOME}/.config/i3status 
 cp ${DOTFILES_DIR}/.gitconfig ${HOME}
 cp ${DOTFILES_DIR}/.latexmkrc ${HOME}
 cp ${DOTFILES_DIR}/.vimrc ${HOME}
 cp ${DOTFILES_DIR}/.Xmodmap ${HOME}
 cp ${DOTFILES_DIR}/.screenrc ${HOME}
+cp ${DOTFILES_DIR}/.xinitrc ${HOME}
