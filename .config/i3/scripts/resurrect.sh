5459701988ba113e9493b61a396b1a497ddfe240
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-01-08 02:41:30 +0900

    update i3 resurrect

---
+++ .config/i3/scripts/resurrect.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eu
+
+I3_RESURRECT_DIR="$HOME/.cache/i3-resurrect/"
+
+if [ $1 == "save" ]; then
+  if [ -n "$(ls $I3_RESURRECT_DIR)" ]; then
+    command rm $I3_RESURRECT_DIR/*
+  fi
+  i3-msg -t get_workspaces | sed 's/},{/\n/g' | awk -F, '{print $3}' | awk -F: '{print $2}' | xargs -n1 i3-resurrect save -d $I3_RESURRECT_DIR -w
+elif [ $1 == "restore" ]; then
+  i3-resurrect ls -d $I3_RESURRECT_DIR | sed -n '/programs/!p' | awk '{print $2}' | xargs -n1 i3-resurrect restore -d $I3_RESURRECT_DIR -w
+else
+  echo "argument: \"save\" or \"restore\""
+  exit 1
+fi
