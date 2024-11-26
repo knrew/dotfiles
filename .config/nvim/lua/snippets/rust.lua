805cebcd37cb39c22b12a05d497de64fc1e3e784
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2024-11-27 04:14:08 +0900

    add nvim snippet

--- .config/nvim/lua/snippets/rust.lua
+++ .config/nvim/lua/snippets/rust.lua
@@ -4,6 +4,7 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
+  -- procon template
   s(
     { trig = "actemplate" },
     {
@@ -12,12 +13,94 @@ return {
         "",
         "fn main() {",
         "    input! {",
+        "        ",
       }),
       i(1),
       t({
-        "}",
+        "",
+        "    }",
         "}",
       }),
+    }
+  ),
+
+  -- グリッド上の移動
+  s(
+    { trig = "acdij" },
+    { t({
+      "const DIJ: [[usize; 2]; 4] = [",
+      "    [1, 0],",
+      "    [0, 1],",
+      "    [1usize.wrapping_neg(), 0],",
+      "    [0, 1usize.wrapping_neg()],",
+      "];",
+    }) }
+  ),
+
+  -- グリッド上の隣接するマスを列挙するイテレータ
+  s(
+    { trig = "acneighboriter" },
+    {
+      t({
+        "DIJ",
+        "    .iter()",
+        "    .map(move |&[di, dj]| (i.wrapping_add(di), j.wrapping_add(dj)))",
+        "    .filter(move |&(i, j)| i < h && j < w)",
+        -- "    "
+      }),
+    }
+  ),
+
+  -- グリッド状で隣接するマスに対するfor
+  s(
+    { trig = "acneighborfor" },
+    {
+      t({
+        "for (ni, nj) in DIJ",
+        "    .iter()",
+        "    .map(move |&[di, dj]| (i.wrapping_add(di), j.wrapping_add(dj)))",
+        "    .filter(move |&(i, j)| i < h && j < w) {",
+        "    "
+      }),
+      i(1),
+      t({
+        "",
+        "}"
+      })
+    }
+  ),
+
+  -- 重みなしグラフ
+  s(
+    { trig = "acgraph1" },
+    {
+      t({
+        "let g = {",
+        "    let mut g = vec![vec![]; n];",
+        "    for &(u, v) in &edges {",
+        "        g[u].push(v);",
+        "        // g[v].push(u);",
+        "    }",
+        "    g",
+        "};"
+      })
+    }
+  ),
+
+  -- 重みつきグラフ
+  s(
+    { trig = "acgraph2" },
+    {
+      t({
+        "let g = {",
+        "    let mut g = vec![vec![]; n];",
+        "    for &(u, v, c) in &edges {",
+        "        g[u].push((v, c));",
+        "        // g[v].push((u, c));",
+        "    }",
+        "    g",
+        "};"
+      })
     }
   ),
 }
