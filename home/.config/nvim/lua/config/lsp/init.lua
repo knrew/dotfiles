922cb465f119a56e5f9e1c64662f23376010ca5c
knrew <rew.7cc.12.1f@gmail.com>
2025-10-05 14:31:00 +0900

    style: fix

--- home/.config/nvim/lua/config/lsp/init.lua
+++ home/.config/nvim/lua/config/lsp/init.lua
@@ -58,9 +58,9 @@ local setup = function()
   vim.lsp.enable(language_servers)
 
   vim.lsp.handlers["textDocument/hover"] =
-      vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
+    vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
   vim.lsp.handlers["textDocument/signatureHelp"] =
-      vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+    vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
   require("lspconfig.ui.windows").default_options.border = "single"
 end
 
