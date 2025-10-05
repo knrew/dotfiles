b286cd18a0d0cb63b3032527e538b944461410f1
knrew <rew.7cc.12.1f@gmail.com>
2025-10-05 12:00:06 +0900

    style: editorconfigに従って修正

--- home/.config/zsh/fzf.zsh
+++ home/.config/zsh/fzf.zsh
@@ -8,87 +8,87 @@ export FZF_DEFAULT_OPTS=" \
 
 # repeat command history
 function fh() {
-	# eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
-	print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+  # eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
 # cd(without hidden directories)
 function fd() {
-	local dir
-	dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d -print 2>/dev/null | fzf +m) &&
-		cd "$dir"
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune -o -type d -print 2>/dev/null | fzf +m) &&
+    cd "$dir"
 }
 
 # cd(including hidden directories)
 function fda() {
-	local dir
-	dir=$(find ${1:-.} -type d 2>/dev/null | fzf +m) && cd "$dir"
+  local dir
+  dir=$(find ${1:-.} -type d 2>/dev/null | fzf +m) && cd "$dir"
 }
 
 # cd(parent directories)
 fdr() {
-	local declare dirs=()
-	get_parent_dirs() {
-		if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
-		if [[ "${1}" == '/' ]]; then
-			for _dir in "${dirs[@]}"; do echo $_dir; done
-		else
-			get_parent_dirs $(dirname "$1")
-		fi
-	}
-	local dir=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
-	cd "$dir"
+  local declare dirs=()
+  get_parent_dirs() {
+    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+    if [[ "${1}" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo $_dir; done
+    else
+      get_parent_dirs $(dirname "$1")
+    fi
+  }
+  local dir=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
+  cd "$dir"
 }
 
 # fuzzy cd from anywhere
 # e.g.) cf word1 word2
 function cf() {
-	local file
-	file="$(locate -Ai -0 $@ | command grep -z -vE '~$' | fzf --read0 -0 -1)"
-	if [[ -n $file ]]; then
-		if [[ -d $file ]]; then
-			cd -- $file
-		else
-			cd -- ${file:h}
-		fi
-	fi
+  local file
+  file="$(locate -Ai -0 $@ | command grep -z -vE '~$' | fzf --read0 -0 -1)"
+  if [[ -n $file ]]; then
+    if [[ -d $file ]]; then
+      cd -- $file
+    else
+      cd -- ${file:h}
+    fi
+  fi
 }
 
 # open file/directory with vim(without hidden files)
 function fe() {
-	local file
-	file=$(find ${1:-.} -path '*/\.*' -prune -o -print 2>/dev/null | fzf +m) &&
-		${EDITOR:-vim} "$file"
+  local file
+  file=$(find ${1:-.} -path '*/\.*' -prune -o -print 2>/dev/null | fzf +m) &&
+    ${EDITOR:-vim} "$file"
 }
 
 # open file/directory with vim(including hidden files)
 function fea() {
-	local file
-	file=$(find ${1:-.} 2>/dev/null | fzf +m) &&
-		${EDITOR:-vim} "$file"
+  local file
+  file=$(find ${1:-.} 2>/dev/null | fzf +m) &&
+    ${EDITOR:-vim} "$file"
 }
 
 # fuzzy vim from anywhere
 function vf() {
-	local files
-	files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1 -m)"})
-	if [[ -n $files ]]; then
-		${EDITOR:-vim} -- $files
-		print -l $files[1]
-	fi
+  local files
+  files=(${(f)"$(locate -Ai -0 $@ | grep -z -vE '~$' | fzf --read0 -0 -1 -m)"})
+  if [[ -n $files ]]; then
+    ${EDITOR:-vim} -- $files
+    print -l $files[1]
+  fi
 }
 
 # kill processes
 function fkill() {
-	local pid
-	if [ "$UID" != "0" ]; then
-		pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
-	else
-		pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
-	fi
-	if [ "x$pid" != "x" ]; then
-		echo $pid | xargs kill -${1:-9}
-	fi
+  local pid
+  if [ "$UID" != "0" ]; then
+    pid=$(ps -f -u $UID | sed 1d | fzf -m | awk '{print $2}')
+  else
+    pid=$(ps -ef | sed 1d | fzf -m | awk '{print $2}')
+  fi
+  if [ "x$pid" != "x" ]; then
+    echo $pid | xargs kill -${1:-9}
+  fi
 }
 
 # ls installed packages
