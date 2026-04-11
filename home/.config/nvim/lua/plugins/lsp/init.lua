ffc513a1561f9ce874d63461d1fa797f57b1ee29
knrew <rew.7cc.12.1f@gmail.com>
2026-04-11 21:04:18 +0900

    chore: nvimのmason系プラグイン参照先をmason-orgに更新

--- home/.config/nvim/lua/plugins/lsp/init.lua
+++ home/.config/nvim/lua/plugins/lsp/init.lua
@@ -3,25 +3,25 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+      "mason-org/mason-lspconfig.nvim",
       "tamago324/nlsp-settings.nvim",
       "nvimtools/none-ls.nvim",
       "jay-babu/mason-null-ls.nvim",
-      "williamboman/mason.nvim",
+      "mason-org/mason.nvim",
     },
     config = function()
       require("plugins.lsp.setup").setup()
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
     lazy = true,
   },
   { "tamago324/nlsp-settings.nvim", lazy = true },
   { "nvimtools/none-ls.nvim", lazy = true },
   { "jay-babu/mason-null-ls.nvim", lazy = true },
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     build = function()
       pcall(function()
         require("mason-registry").refresh()
