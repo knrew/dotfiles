6ca4f1630516673b2df9a4fbe70e1296da6b672f
SUGAWARA Ryunosuke <rew.7cc.12.1f@gmail.com>
2026-07-06 23:41:36 +0900

    feat: add herdr configuration (#37)

--- home/.config/zsh/integrations.zsh
+++ home/.config/zsh/integrations.zsh
@@ -2,4 +2,13 @@ STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 [[ -f $STARSHIP_CONFIG ]] && export STARSHIP_CONFIG
 ((${+commands[starship]})) && eval "$(starship init zsh)"
 
-((${+commands[zellij]})) && eval "$(zellij setup --generate-auto-start zsh)"
+#((${+commands[zellij]})) && eval "$(zellij setup --generate-auto-start zsh)"
+
+# if [[ -o interactive && -t 0 && -t 1 ]] \
+#   && [[ "${HERDR_AUTO_START:-1}" != "0" ]] \
+#   && [[ "${HERDR_ENV:-0}" != "1" && -z "$HERDR_PANE_ID" ]] \
+#   && [[ -z "$TMUX" && -z "$ZELLIJ" && -z "$ZELLIJ_SESSION_NAME" ]] \
+#   && [[ "$TERM" != "dumb" ]] \
+#   && ((${+commands[herdr]})); then
+#   exec herdr
+# fi
